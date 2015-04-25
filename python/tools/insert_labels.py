5e52c28f451ccde26221d0851e0ba3851780053d
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2015-04-25 12:05:08 +0200

    reordered python data

--- python/tools/insert_labels.py
+++ python/tools/insert_labels.py
@@ -18,23 +18,29 @@ def slicingToString(slicing):
     strSlicing += ']'
     return strSlicing
 
+
+# the new file should already exist and have the labels added, but not drawn (if drawn they will be deleted)
+
 def transfer(labelfile, new_project_filepath):
 	labelimage = vigra.readHDF5(labelfile, "labels")
+	labelimage = np.expand_dims( labelimage, axis = 3)
+	#labelbinary = (labelimage>0).astype(np.uint8)
 	
-	with h5py.File(new_project_file_filepath, 'a') as project_file:
+	with h5py.File(new_project_filepath, 'a') as project_file:
 		if 'PixelClassification/LabelSets' in project_file:
-			del project_files['PixelClassification/LabelSets']
+			# start from scratch: delete all previous labels
+			del project_file['PixelClassification/LabelSets']
 		labelset_group = project_file.create_group('PixelClassification/LabelSets')
 		
-		print labelbinary.shape
+		print "big block shape", labelimage.shape
 		label_group_name = 'labels000'
-		label_group = labelset_group(label_group_name)
-		dataset = label_group.create_dataset( "block0000", data = labelbinary)
+		label_group = labelset_group.create_group(label_group_name)
+		dataset = label_group.create_dataset( "block0000", data = labelimage)
 		print dataset.shape
 		
 		slicing = [slice(0, stop) for stop in dataset.shape]
-		print slicing, slicingToString(slicing, numpy.sum(dataset))
-		dataset.attrs["blockSlice"] = slcingToString(slicing)
+		print slicing, slicingToString(slicing), np.sum(dataset)
+		dataset.attrs["blockSlice"] = slicingToString(slicing)
 
 		
 
